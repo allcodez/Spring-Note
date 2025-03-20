e3b25dba9de398f08e9868e6321502f6118c0864
michaelarmando1 <michaelamando25@gmail.com>
2025-03-20 23:31:17 +0100

    New Changes

--- NoteApp/src/main/java/com/example/NoteApp/Model/Note.java
+++ NoteApp/src/main/java/com/example/NoteApp/Model/Note.java
@@ -14,4 +14,41 @@ public class Note {
             strategy = GenerationType.SEQUENCE,
             generator = "user_id_sequence"
     )
+
+    private int id;
+    private String title;
+    private String content;
+
+    public Note() {
+    }
+
+    public Note(int id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
